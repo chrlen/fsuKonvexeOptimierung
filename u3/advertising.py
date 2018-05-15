174394838e9a5364a2261b5fbfc4200969d7c474
Christian Lengert <clengert@googlemail.com>
2018-05-15 07:36:08 +0200

    finish advertising.py

--- u3/advertising.py
+++ u3/advertising.py
@@ -1,8 +1,36 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np 
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("Advertising.csv")
-plt.scatter(data[:,0],data[:,1])
+eta = data['Sales']
+etaMean = eta.mean()
+meanFreeEta = eta -etaMean
+
+
+for columnName in ['TV','Radio','Newspaper']:
+    xi = data[columnName]
+    xiMean = xi.mean()
+    meanFreeXi = xi - xiMean
+    
+    #Parameterschätzung
+    x_1 = sum(meanFreeXi * meanFreeEta) / sum(np.power(meanFreeXi,2))
+    x_2 = etaMean -x_1*xiMean
+    
+    #Erzeuge Scatterplot
+    plt.scatter(data[columnName],data['Sales'],color='C2')
+    
+    #Berechne Funktionswerte für Regressionsgrade 
+    #auf dem x-Interval des Scatterplots
+    axes = plt.gca()
+    x_axis = np.array(axes.get_xlim())
+    regVals = x_1 * x_axis + x_2
+    plt.plot(x_axis,regVals,'--',color='C3')
+    
+    plt.title(columnName)
+    plt.show()
+
+
 
 
 
