fd67bae679d312d2d6b1b759ce92b5947717f505
Christian Lengert <clengert@googlemail.com>
2018-05-17 17:45:09 +0200

    progress

--- u4/descent.py
+++ u4/descent.py
@@ -1,11 +1,37 @@
 import numpy as np
-#import pandas as pd
+import numpy.linalg as npl
+import matplotlib.pyplot as plt
 
-def simpleGradientDescent(Q,q,c,tol):
+def crit1(fx_this,fx_next,epsilon):
+    return(fx_this -fx_next <= epsilon * np.max(1,np.abs(fx_this)))
+
+def crit2(fx_this,fx_next,epsilon2):
+    return(fx_next - fx_this <= epsilon2 * np.max(1,npl.norm(fx_this)))
+
+#def crit3(fx_this,fx_next,epsilon2):
+#    return(fx_next - fx_this <= epsilon2 * np.max(1,np.abs(fx_this)))
+
+def simpleGradientDescent(Q,q,c,epsilon,plot=TRUE,verbose=TRUE):
     print("done!")
 
-def conjugateGradientDescent(Q,q,c,tol):
+def conjugateGradientDescent(Q,q,c,epsilon,plot=TRUE,verbose=TRUE):
     print("done!")    
     
+Q = np.diag([2,2,1,1,0.5])
+q = np.array([-4,-4,-2,-2,-1])
+c = np.array([6.5])
+print("The Function is:")
+print("Q = " + str(Q))
+print("q = " + str(q))
+print("c = " + str(c))
 
-startAt = numpy.array([0,0])
+startAt = np.array([0,0])
+print("Starting at: " +str(startAt))
+
+simpleGradientOptimum = simpleGradientDescent(Q,q,c,0.00001)
+conjugateGradientOptimum = conjugateGradientDescent(Q,q,c,0.00001)
+
+
+
+
+
