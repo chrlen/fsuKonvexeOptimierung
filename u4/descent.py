412a779319100857d8a1ca433c6d0753344f65c6
Christian Lengert <clengert@googlemail.com>
2018-05-17 13:45:18 +0200

    begin u4

---
+++ u4/descent.py
@@ -0,0 +1,11 @@
+import numpy as np
+#import pandas as pd
+
+def simpleGradientDescent(Q,q,c,tol):
+    print("done!")
+
+def conjugateGradientDescent(Q,q,c,tol):
+    print("done!")    
+    
+
+startAt = numpy.array([0,0])
