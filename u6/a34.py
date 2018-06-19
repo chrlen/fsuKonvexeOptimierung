1e6ae07559b657f2692996aa3d11962962516c6b
Christian Lengert <clengert@googlemail.com>
2018-06-19 14:18:46 +0200

    finish gradientchecks

--- u6/a34.py
+++ u6/a34.py
@@ -1,1 +1,6 @@
-import functions
+import descent as dc
+import functions as fct
+import numpy as np
+
+cg = dc.checkGradient(np.array([100,100]),fct.regressionFct,fct.regressionGradient)
+ch = dc.checkHessian(np.array([100,100]),fct.regressionFct,fct.regressionHessian)
